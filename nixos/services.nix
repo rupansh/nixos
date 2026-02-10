e1ed3891ab341ad619a366c3ef12d953817e4385
rupansh <rupanshsekar@hotmail.com>
2026-02-10 23:29:58 +0530

    add avahi and tailscale

--- nixos/services.nix
+++ nixos/services.nix
@@ -2,4 +2,11 @@
   services.gvfs.enable = true;
   services.udisks2.enable = true;
   services.upower.enable = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+  };
+
+  services.tailscale.enable = true;
 }
