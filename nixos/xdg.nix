4a882a64257ac1e40d70a8970a0d455736d08c45
rupansh <rupanshsekar@hotmail.com>
2026-05-30 08:17:46 +0530

    fix(xdg): fix for background portal

--- nixos/xdg.nix
+++ nixos/xdg.nix
@@ -2,12 +2,18 @@
 {
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
     config = {
-      hyprland.default = [
-        "hyprland"
-        "gtk"
-      ];
+      hyprland = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Background" = [ "gnome" ];
+      };
     };
   };
 }
